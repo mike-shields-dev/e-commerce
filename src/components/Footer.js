c84c3254e253a6e270f6e847bc8d890f8a7178ea
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-02 15:58:19 +0100

    Add whitespace

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,9 @@ const Footer = () => {
     <Wrapper>
       <h5>
         &copy; {new Date().getFullYear()}
-        <span>Comfy Sloth</span>
+        <span> Comfy Sloth</span>
       </h5>
-      <h5>All rights reserved</h5>
+      <h5> All rights reserved</h5>
     </Wrapper>
   );
 }
