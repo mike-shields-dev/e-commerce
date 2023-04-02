3e992ad4d6fa5f2073ea5d3e1f3d109979caf4e6
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-02 15:44:39 +0100

    Add title, links and toggle button to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,27 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return (
+    <NavContainer>
+      <div className='nav-center'>
+      <div className='nav-header'>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+          <button type="button" className="nav-toggle">
+            <FaBars />
+          </button>
+      </div>
+        <ul className="nav-links">
+          {links.map(({id, text, url}) => 
+            <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          )}
+        </ul>
+      </div>
+    </NavContainer>
+  );
 }
 
 const NavContainer = styled.nav`
