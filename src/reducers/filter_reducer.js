fdf493e32dbacd4cf7e6ce990e95870724c009ac
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-05 11:28:51 +0100

    chore: Setup filter_context state management and wrap App in provider

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -10,8 +10,16 @@ import {
 } from '../actions'
 
 const filter_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
+  switch(action.type) {
+    case LOAD_PRODUCTS:
+      return {
+        ...state, 
+        all_products: [...action.payload],
+        filtered_products: [...action.payload],
+      }
+    default: 
+      return state;
+  }
 }
 
-export default filter_reducer
+export default filter_reducer;
