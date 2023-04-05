37340d42417a4eceb4e52ba66ed94b5ffde9566b
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-05 09:01:14 +0100

    feat: Add content and logic to Stars and render in SingleProductsPage

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,8 +1,31 @@
 import React from 'react'
 import styled from 'styled-components'
 import { BsStarFill, BsStarHalf, BsStar } from 'react-icons/bs'
-const Stars = () => {
-  return <h4>stars</h4>
+
+const Stars = ({ reviews, stars }) => {
+  console.log(stars);
+  const starsDisplay = Array.from({length: 5}, (_, index) => {
+    const number = index + 0.5;
+
+    return (
+      <span key={index}>
+        {
+          stars >= index + 1
+          ? <BsStarFill />
+          : stars >= number
+            ? <BsStarHalf />
+            : <BsStar />
+        }
+      </span>
+    )
+  });
+
+  return (
+    <Wrapper>
+      <div className="stars">{starsDisplay}</div>
+      <p>({reviews} customer reviews)</p>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
