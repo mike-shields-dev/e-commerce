7eebb495146632321ed6d07dbf4a919d1012c632
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-12 11:17:57 +0100

    feat: Add switch case ADD_TO_CART action & logic for new item

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -1,14 +1,49 @@
-import {
+import * as actions from '../actions';
+const {
   ADD_TO_CART,
   CLEAR_CART,
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} = actions;
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
+  if(!(action.type in actions)) {
+    throw new Error(`No Matching "${action.type}" - action type`)
+  }
+
+  switch(action.type) {
+    case ADD_TO_CART: {
+      const {id, colour, amount, product} = action.payload;
+
+      const existingItem = state.cart.find(existingItem => 
+        existingItem.id === id + colour
+      );
+
+      if(existingItem) {
+        return {
+          ...state,
+          cart: [...state.cart],
+        }
+      } else {
+        const newItem = {
+          id: id + colour,
+          name: product.name,
+          colour,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max:product.stock,
+        }
+        return {
+          ...state, 
+          cart: [...state.cart, newItem]
+        }
+      }
+    }
+    default: 
+      return state;
+  }
 }
 
 export default cart_reducer
