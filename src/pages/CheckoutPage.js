4115308210318169d4f0ac28549bb2f688519f55
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-04-15 14:24:46 +0100

    feat: Implement basic dummy card payment with stripe

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import styled from 'styled-components'
-import { PageHero, StripeCheckout } from '../components'
-// extra imports
+import { PageHero, StripeCheckout } from '../components' 
 import { useCartContext } from '../context/cart_context'
 import { Link } from 'react-router-dom'
 
 const CheckoutPage = () => {
+  const { cart } = useCartContext();
+
   return (
     <main>
       <PageHero title="checkout" />
       <Wrapper className="page">
-        <h1>Checkout Here</h1>
+        {cart.length > 0 
+          ? 
+          <StripeCheckout />
+          : 
+          <div className='empty'>
+            <h2>your cart is empty</h2>
+            <Link className="btn" to="/products">view products</Link>
+          </div>
+        }
       </Wrapper>
     </main>
   );
 }
-const Wrapper = styled.div``
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`
 
 export default CheckoutPage
